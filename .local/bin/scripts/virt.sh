dd7b646626c98e6c9980e63671c9492b23c79f86
minaalbert33 <mina.albert33@gmail.com>
2024-01-28 01:31:17 +0200

    Move scripts into .local/bin

--- .local/bin/scripts/virt.sh
+++ .local/bin/scripts/virt.sh
@@ -6,7 +6,7 @@
 DiskImages=$(ls ~/vms/)
 
 # isoImage=$(echo -e "$IsoImages" | dmenu -fn "$dmenufont" -i -p "Choose an Iso Image:")
-diskImage=$(echo -e "$DiskImages" | dmenu -fn "$dmenufont" -i -p "Choose a Disk Image:")
+diskImage=$(echo -e "$DiskImages" | rofi -dmenu  -p "Choose a Disk Image:")
 
 command="qemu-system-x86_64 -enable-kvm -boot menu=on -drive file=vms/"$diskImage" -m 2G -cpu host -smp 4 -vga virtio -display sdl,gl=on "
 
