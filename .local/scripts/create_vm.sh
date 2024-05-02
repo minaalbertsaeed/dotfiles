11929f5f701063d1dd04be603a2034e88469df33
minaalbert33 <mina.albert33@gmail.com>
2024-05-02 15:07:55 +0300

    Add this line for abdola

--- .local/scripts/create_vm.sh
+++ .local/scripts/create_vm.sh
@@ -3,3 +3,4 @@
 iso_images=" $(ls -l /home/mina/hdd/D/IsoImages/ | awk '{print $9}')"
 selected_iso=$(echo "$iso_images" | dmenu -i -fn "$dmenufont" -p "Choose an Iso Image: ")
 # echo $selected_iso
+https://davidaugustat.com/linux/how-to-compile-linux-kernel-on-ubuntu
