32824ffd1651fc29c436ea4e5864ef782aa49dda
minaalbertsaeed <mina.albert33@gmail.com>
2024-09-07 15:28:02 +0300

    Edit st & dwm

--- src/st/config.h
+++ src/st/config.h
@@ -100,44 +100,40 @@ float alpha = 0.95;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"#0adc61",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#45475A",
+	"#F38BA8",
+	"#A6E3A1",
+	"#F9E2AF",
+	"#89B4FA",
+	"#F5C2E7",
+	"#94E2D5",
+	"#FAF9F6",
+
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"#d2e0de",
-	"yellow",
-	"#3dbc61",
-	"magenta",
-	"cyan",
-	"white",
+	"#585B70",
+	"#F38BA8",
+	"#A6E3A1",
+	"#F9E2AF",
+	"#89B4FA",
+	"#F5C2E7",
+	"#94E2D5",
+	"#FAF9F6",
 
-  [256] = "#161616", /* background */
-  [257] = "#d2e0de", /* foreground */
+[256] = "#FAF9F6", /* default foreground colour */
+[257] = "#16181d", /* default background colour */
+[258] = "#FAF9F6", /*575268*/
+// ", /*575268*/
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
 };
 
 
 /*
- * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 257;
-
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
  * Default style of cursor
